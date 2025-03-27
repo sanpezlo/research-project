f3be8ec9842d43d1b47709801c831a2aecfb7999
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2025-03-27 14:27:20 +0000

    feat: add problem 25

---
+++ data/problems/25/tests.js
@@ -0,0 +1,33 @@
+const functions = [];
+
+function all() {
+  for (let i = 0; i < functions.length; i++) {
+    const f = functions[i];
+
+    const tests = [];
+    tests.push(test(f, 0, 0));
+    tests.push(test(f, 1, 1));
+    tests.push(test(f, 10, 2));
+    tests.push(test(f, 11, 3));
+    tests.push(test(f, 100, 4));
+    tests.push(test(f, 101, 5));
+    tests.push(test(f, 110, 6));
+    tests.push(test(f, 111, 7));
+    tests.push(test(f, 1000, 8));
+    tests.push(test(f, 1010, 10));
+    tests.push(test(f, 1111, 15));
+    tests.push(test(f, 10000, 16));
+    tests.push(test(f, 11001, 25));
+    tests.push(test(f, 11111111, 255));
+
+    if (!tests.every(Boolean)) {
+      console.log(i, tests);
+    }
+  }
+}
+
+function test(f, input, output) {
+  return JSON.stringify(f(input)) == JSON.stringify(output);
+}
+
+all();
--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,7 @@ from md import read
 from gen_code import gen
 from gen_tests import read_lines, gen_tests, write_lines
 
-id_problem = "24"
+id_problem = "25"
 
 def dataframe():
     md = read(f"/workspaces/research-project/data/problems/{id_problem}/correct.md")
